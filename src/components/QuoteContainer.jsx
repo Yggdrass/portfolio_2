84e393db5ecf949abfb3dd0a9f5e00314dc1d917
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-12-12 20:04:58 +0100

    Added a few base pages and some components to get the browser router to work and display the header on each page and also a 404 error page.

---
+++ src/components/QuoteContainer.jsx
@@ -0,0 +1,13 @@
+const QuoteContainer = () => {
+  return (
+    <div className="quote_container">
+      <h1 className="quote_text">
+        "Good design is like a refrigerator. When it works no one notices, but
+        when it doesn't, it sure stinks"
+      </h1>
+      <h2 className="quote_author">Irene Au</h2>
+    </div>
+  );
+};
+
+export default QuoteContainer;
